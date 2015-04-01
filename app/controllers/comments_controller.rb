ab6bf51dff791c54831f265df4db974980a21f5a
axton21 <axton_n@yahoo.com>
2015-04-01 12:17:53 -0400

    fixed strong params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,15 @@
 class CommentsController < ApplicationController
 
 	def create
-		@post = Post.find(params[:post_id])
-		@post.comments << Comment.create(content: params[:Content])
+		@post = Post.find(comment_params[:post_id])
+		@post.comments.create(content: comment_params[:content])
 		@post.save
 		redirect_to "/posts/#{@post.id}"
 	end
 private
 
 def comment_params
-	params.require(:post).permit(:post_id, comments_attributes: [:content])
+	params.require(:comment).permit(:content, :post_id)
 end
 
 end
